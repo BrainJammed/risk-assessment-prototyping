4b02054e31ab0999b38fa539be1ff7b7386358e0
Paul Coupe <paul.coupe@digital.justice.gov.uk>
2020-08-31 17:59:15 +0100

    prototype build wip

--- app/routes.js
+++ app/routes.js
@@ -7,4 +7,35 @@ router.get('/dashboard', function (req, res) {
   res.render('offender-dashboard', { message: 'Hello world' })
 })
 
+// prototype v2 pages
+router.get('/proto-v2/subject', function (req, res) {
+  res.render('proto-v2/subject.njk')
+})
+router.post('/subject', function (req, res) {
+  res.redirect('/proto-v2/longterm')
+})
+router.get('/proto-v2/longterm', function (req, res) {
+  res.render('proto-v2/longterm.njk')
+})
+router.post('/longterm', function (req, res) {
+  res.redirect('proto-v2/positives')
+})
+router.get('/proto-v2/positives', function (req, res) {
+  res.render('proto-v2/positives.njk')
+})
+router.post('/positives', function (req, res) {
+  res.redirect('proto-v2/people-affected')
+})
+
+router.get('/proto-v2/people-affected', function (req, res) {
+  res.render('proto-v2/people-affected.njk')
+})
+router.post('/people-affected', function (req, res) {
+  res.redirect('proto-v2/effect-indiv')
+})
+
+router.get('/proto-v2/effect-indiv', function (req, res) {
+  res.render('proto-v2/effect-indiv.njk')
+})
+
 module.exports = router
